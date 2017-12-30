d11a5be144af4b4e387e3dcbfa4887177662afa5
unknown <brandonkho1@berkeley.edu>
2017-12-30 14:05:47 -0800

    added post email endpoint

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/email', (req, res) => res.send('Email endpoint working'));
-
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Example app listening on port 3000!'));
+
+app.get('/email', (req, res) => res.send('Email endpoint working'));
+
+app.post('/email', (req, res) => {
+	var email = req.body;
+	var to = email.to;
+	var subject = email.subject;
+	var body = email.body;
+	
+	res.send('Got a POST request');
+});
